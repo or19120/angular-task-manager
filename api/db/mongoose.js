c6755c98edcf7a5c36003dbd9a7e13e79ee3113b
Or Shoham <or19120@gmail.com>
2020-07-14 10:37:35 +0300

    first commit

---
+++ api/db/mongoose.js
@@ -0,0 +1,21 @@
+// this file will handle the connection logic to mongodb
+const mongoose = require("mongoose");
+
+mongoose.Promise = global.Promise;
+mongoose
+  .connect(
+    // .connect("mongodb://localhost:27017/TaskManager"
+    "mongodb+srv://or19120:Oro191200@cluster0-qi9bu.gcp.mongodb.net/Todo?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("Connected to db!");
+  })
+  .catch((e) => console.log("Error: " + e));
+
+module.exports = mongoose;
